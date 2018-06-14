8868bcc1391268f86abe6d2e138053253e46b47f
gross1989 <gross1989@zedat.fu-berlin.de>
2018-06-14 12:19:12 +0200

    make use of prepared statements

--- simple_demo.php
+++ simple_demo.php
@@ -28,11 +28,12 @@ if (empty($_GET)) {
 // add the new entry to the table
 else {
 
-    $sql = "INSERT INTO organisationen VALUES("
-        . $myPDO->quote($_GET['organisation']) . ','
-        . $myPDO->quote($_GET['branche']) . ','
-        . $_GET['mitarbeiter'] . ','
-        . $_GET['umsatz'] . ")";
+    $stmt = $myPDO->prepare("INSERT INTO organisationen VALUES (?, ?, ?, ?)");
 
-    $result = $myPDO->query($sql);
+    $stmt->bindParam(1, $_GET['organisation']);
+    $stmt->bindParam(2, $_GET['branche']);
+    $stmt->bindParam(3, $_GET['mitarbeiter']);
+    $stmt->bindParam(4, $_GET['umsatz']);
+
+    $stmt->execute();
 }
