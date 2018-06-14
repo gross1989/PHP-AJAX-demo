22eeb5f7d10f3e8f51db5127eefda0389189ef4d
gross1989 <gross1989@zedat.fu-berlin.de>
2018-06-14 12:39:39 +0200

    OO design

--- simple_demo.php
+++ simple_demo.php
@@ -1,39 +1,55 @@
 <?php
 
-$myPDO = new PDO('pgsql:host=localhost;dbname=postgres_db', 'postgres_user', 'postgres_user');
+class SimpleDemo {
 
-// list all entries from the table
-if (empty($_GET)) {
+    private $myPDO;
 
-    $sql = "SELECT * FROM organisationen";
-    $result = $myPDO->query($sql);
-
-    echo "<table>
-    <tr>
-    <th>Organisation</th>
-    <th>Branche</th>
-    <th>Mitarbeiter</th>
-    <th>Umsatz</th>
-    </tr>";
-    while($row = $result->fetch()) {
-        echo "<tr>";
-        echo "<td>" . $row['organisation'] . "</td>";
-        echo "<td>" . $row['branche'] . "</td>";
-        echo "<td>" . $row['mitarbeiter'] . "</td>";
-        echo "<td>" . $row['umsatz'] . "</td>";
-        echo "</tr>";
+    public function __construct() {
+        $this->myPDO = new PDO('pgsql:host=localhost;dbname=postgres_db', 'postgres_user', 'postgres_user');
     }
-    echo "</table>";
+
+    // list all entries from the table
+    public function list() {
+        $sql = "SELECT * FROM organisationen";
+        $result = $this->myPDO->query($sql);
+
+        echo "<table>
+        <tr>
+        <th>Organisation</th>
+        <th>Branche</th>
+        <th>Mitarbeiter</th>
+        <th>Umsatz</th>
+        </tr>";
+        while($row = $result->fetch()) {
+            echo "<tr>";
+            echo "<td>" . $row['organisation'] . "</td>";
+            echo "<td>" . $row['branche'] . "</td>";
+            echo "<td>" . $row['mitarbeiter'] . "</td>";
+            echo "<td>" . $row['umsatz'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+
+    // add the new entry to the table
+    public function addEntry() {
+        $stmt = $this->myPDO->prepare("INSERT INTO organisationen VALUES (?, ?, ?, ?)");
+
+        $stmt->bindParam(1, $_GET['organisation']);
+        $stmt->bindParam(2, $_GET['branche']);
+        $stmt->bindParam(3, $_GET['mitarbeiter']);
+        $stmt->bindParam(4, $_GET['umsatz']);
+
+        $stmt->execute();
+    }
+
 }
-// add the new entry to the table
-else {
 
-    $stmt = $myPDO->prepare("INSERT INTO organisationen VALUES (?, ?, ?, ?)");
 
-    $stmt->bindParam(1, $_GET['organisation']);
-    $stmt->bindParam(2, $_GET['branche']);
-    $stmt->bindParam(3, $_GET['mitarbeiter']);
-    $stmt->bindParam(4, $_GET['umsatz']);
+$demo = new SimpleDemo();
 
-    $stmt->execute();
+if (empty($_GET)) {
+    $demo->list();
+} else {
+    $demo->addEntry();
 }
